9c67172b0e876aa63aa13c6033a98c0f5149e26a
bala-amypo <bala@amypo.in>
2025-12-22 08:27:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/SkillCategoryDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillCategoryDto {
+    private Long id;
+    private String categoryName;
+    private String description;
+}
---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String location;
+    private String role;
+    private Double rating;
+}
