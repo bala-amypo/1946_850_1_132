45b7bce16e0519823c15c00c524280c63a6ad965
bala-amypo <bala@amypo.in>
2025-12-26 12:29:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.SkillCategory;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface SkillCategoryRepository extends JpaRepository<SkillCategory, Long> {
-// }
--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.service;
-import org.springframework.stereotype.Service;
-@Service
-public class UserService {} // Placeholder for Mock
